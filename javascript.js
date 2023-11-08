eb2ab96b20a37f4a8b261c49e3759194a3ba327e
Esmeralda Torres Aguirre <esmeraldatorresaguirre@MacBook-Air-de-Esmeralda.local>
2023-11-07 23:40:52 -0600

    replace

--- javascript.js
+++ javascript.js
@@ -4,20 +4,20 @@ const playPause = document.getElementById("play");
 function abrir() {
   document.getElementById("sobre-arriba").classList.add("animate__fadeOutUp");
   document.getElementById("sobre-abajo").classList.add("animate__slideOutDown");
-  document.getElementById("logo").classList.remove("animate__pulse");
+  document.getElementById("logo").classList.remove("animate__infinite");
+
   document.getElementById("parent-div").classList.remove("test-class");
   document.getElementsByTagName("body")[0].classList.remove("avoiding-scroll");
 
   window.scrollTo(0, 0);
   setTimeout(function () {
+    audio.play();
+  }, 10);
+  setTimeout(function () {
     document.getElementById("logo").classList.add("hide");
     document.getElementById("hoja-principal").classList.add("hide");
     document.getElementById("invitacion").classList.remove("hide");
-    document.getElementById("name").classList.add("animate__zoomIn");
   }, 2500);
-  setTimeout(function () {
-    audio.play();
-  }, 2000);
 }
 
 // auidio
@@ -36,7 +36,7 @@ playPause.addEventListener("click", () => {
 
 // Cuenta regresiva -------------------------------------------------------------------
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 01, 2022 15:00").getTime();
+var countDownDate = new Date("Dec 09, 2023 16:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -55,7 +55,7 @@ var x = setInterval(function () {
   // Output the result in an element with id="demo"
   document.getElementById("demo").innerHTML = `
   <div class="d-flex justify-content-center">
-    <div class="cuenta pb-4 px-4" >
+    <div class="cuenta p-4" >
   <span>${days} días</span>
   <span class="ml-2 mr-2"> : </span>  
   <span>${hours} hrs</span> 
